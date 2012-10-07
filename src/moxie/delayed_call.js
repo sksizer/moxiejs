79645236e833bf967a99e344e8138a43f33cb675
Shane Sizer <shane@sizerllc.com>
2012-10-07 13:02:40 -0600

    Added documentation to DelayedCall

--- src/moxie/delayed_call.js
+++ src/moxie/delayed_call.js
@@ -20,13 +20,33 @@
  */
 
 define(function() {
-    var DEFAULT_DELAY = 250;
+    var DEFAULT_DELAY = 250; // in ms
 
+    /**
+     * DelayedCall is useful for 'coallescing' a series of multiple triggers 
+     * into only a single resultant function call.
+     * 
+     * @param config When constructing a delayedCall a user should supply a
+     * configuration object with the following key values:
+     * 
+     * - targetFunction (required) - this is the function that will be called 
+     * after the delayedCall is triggered.
+     * - delay (optional, defaults to 250ms) - this is the amount of time the
+     * DelayedCall will wait after the last trigger before calling the 
+     * targetFunction.
+     * - args (optional, defaults to null) - this is a bound array of arguments
+     * to be applied to the targetFunction when/if it gets called.
+     * 
+     * @constructor
+     */
     var DelayedCall = function(config) {
-        if (config.hasOwnProperty('targetFunction'))
+        if (config.hasOwnProperty('targetFunction')) {
             this._targetFunction = config['targetFunction'];
-        else
-            throw new Error('Delayed call must provide target function!');
+        } else {
+            throw new Error(
+                'DelayedCall MUST have a targetFunction supplied in it\'s ' +
+                'constructor config!');
+        }
         this._delay = config.hasOwnProperty('delay') ? config['delay'] : DEFAULT_DELAY;
         this._thisObject = config.hasOwnProperty('thisObject') ? config['thisObject'] : null;
         this._args = config.hasOwnProperty('args') ? config['args'] : null;
@@ -34,16 +54,33 @@ define(function() {
     }
 
     DelayedCall.prototype = {
+        /**
+         * 
+         */
         trigger: function() {
             if (this._timer) {
                 clearTimeout(this._timer);
                 this._timer = null;
             }
+            // TODO - function.bind is not available in all browsers
             this._timer = setTimeout(this.fire.bind(this), this._delay);
         },
+        /**
+         * Causes the bound target function to be called.  Typically this is 
+         * called indirectly by calling trigger.
+         * @param event
+         */
         fire: function(event) {
             this._targetFunction.apply(this._thisObject, this._args);
         },
+        /**
+         * Clears any currently running delayedCall and unbinds all variables 
+         * and references the DelayedCall has to the targetFunction and supplied
+         * arguments.  
+         * 
+         * It is recommended to run this when finished with the delayedCall to 
+         * help avoid memory leaks and simplify GC of user agent. 
+         */
         dispose: function() {
             if (this._timer) {
                 clearTimeout(this._timer);
